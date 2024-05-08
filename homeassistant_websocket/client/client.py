d9adabd3415eeab59b311385e1a453034bff12a4
Erik <erik_easy@hotmail.com>
2024-05-08 22:51:49 +0200

    Client mostly finished + logging. Video processing has motion detection and questionable face recognition

--- homeassistant_websocket/client/client.py
+++ homeassistant_websocket/client/client.py
@@ -1,88 +1,147 @@
 # main client module
 import asyncio
+import logging
 import json
 import websockets
 import base64
 import cv2
 import numpy as np
+import psutil
 from client.video_processing import VideoProcessor
+from datetime import datetime
 
 
 class Client:
-    def __init__(self, client_id: int, uri: str):
+    def __init__(self, client_id: int, uri: str, status: int):
         self.uri = uri
         self.client_id = client_id
+        self.status = status
 
-    async def process(self, frame, command):
-        # Process the frame according to the command
-        if command == 'motion_detection':
-            VideoProcessor.motion_detection(frame)
-        else:
-            VideoProcessor.process_video(frame)
-        # Add more options as needed
+    """ abstraction
+    async def send_message(websocket, message):
+        try:
+            await websocket.send(message)
+        except Exception as e:
+            logging.error(f"Error occurred while sending message: {e}")
+    """
 
-        return None  # Return the result of the processing
+    """ If we need to send the videofeed forward
+    async def video_forwarded(self, websocket):
+        try:
+        # Forward the livestream to the server
+            cap = cv2.VideoCapture(0)  # Capture frames from camera 0 
+            while True:
+                ret, frame = cap.read()
+                if not ret:
+                    break
+
+                # Encode frame to base64
+                _, buffer = cv2.imencode('.jpg', frame)
+                frame_base64 = base64.b64encode(buffer)
+
+                # Send frame to the server
+                await websocket.send(frame_base64)
+
+            cap.release()
+        except Exception as e:
+            logging.error(f"Error occurred during livestream forwarding: {e}")
+    """
+    async def get_cpu_usage(self):
+        cpu_usage = psutil.cpu_percent()
+        return cpu_usage
+    
+    async def update_status(self, websocket):
+        self.status = await self.get_cpu_usage()
+        if self.status > 80:
+            await websocket.send(f"Caution! Client {self.client_id} working at {self.status}%")
+
+    async def process(self, frame, command, message):
+        try: 
+            # Process the frame according to the command
+            if command == 'motion_detection':
+                VideoProcessor.motion_detection(frame)
+            elif command == 'detect_motion':
+                if VideoProcessor.motion_detected is True:
+                    message = "Motion detected"
+                    return message
+                    # await self.video_forwarded(websocket)
+            else:
+                VideoProcessor.process_video(frame)
+            # Add more options as needed
+
+            return None  # Return the result of the processing
+        except Exception as e:
+            logging.error(f"Error occurred during processing: {e}")
+            return None
 
     async def start_listening(self, websocket):
-        response = await websocket.recv()
-        print(f"< Response from server: {response}")  # Pong
+        try:
+            self.ping(websocket)
+            await self.update_status(websocket)
+            response = await websocket.recv()
+            print(f"< Response from server: {response}")  # Pong
 
-        if response.startswith("CMD"):  # Check response start string
-            command = response.split(" ", 1)[1]  # Extract the command
-            print("Received a command processing request.")
-        else:
-            # Decode the frame
-            b64_decoded = base64.b64decode(response)
-            np_data = np.frombuffer(b64_decoded, dtype=np.uint8)
-            frame = cv2.imdecode(np_data, flags=1)
-
-            # Process the frame
-            result = await self.process(frame, command)
-            print("Frame has been processed.")
-
-            if result:
-                print("Sending success message to server...")
-                await websocket.send(f"RESULT{self.client_id}: Success => {result}")
+            if response.startswith("CMD"):  # Check response start string
+                command = response.split(" ", 1)[1]  # Extract the command
+                print("Received a command processing request.")
             else:
-                print("Sending failure message to server...")
-                await websocket.send(f"RESULT{self.client_id}: Fail => {result}")
+                # Decode the frame
+                b64_decoded = base64.b64decode(response)
+                np_data = np.frombuffer(b64_decoded, dtype=np.uint8)
+                frame = cv2.imdecode(np_data, flags=1)
 
+                # Process the frame
+                result = await self.process(frame, command)
+                print("Frame has been processed.")
+
+                if result:
+                    print("Sending success message to server...")
+                    await websocket.send(f"RESULT{self.client_id}: Success => {result}")
+                else:
+                    print("Sending failure message to server...")
+                    await websocket.send(f"RESULT{self.client_id}: Fail => {result}")
+        except Exception as e:
+            logging.error(f"Error occurred during the communication with server: {e}")
+    
     async def connect(self):
         async with websockets.connect(self.uri) as websocket:
             try:
                 while True:
                     await self.start_listening(websocket)
             except KeyboardInterrupt as e:
-                print("Closing Client")
+                print(f"Closing Client: {e}")
             finally:
                 await websocket.close()
 
     async def ping(websocket, ping_pong_interval_sec=10, message="keep going"):
-        await websocket.send(message)
-        response = await websocket.recv()
-        print(f"< Response from the server: {response}")
-        await asyncio.sleep(ping_pong_interval_sec)
-
-    async def send_command(self):
-        pass  #Maybe this function should just be a filter, depending on how we send commands
-    
-    async def send_message(self, websocket, data):
-        message = data
-        await websocket.send(data)
-        print(f'Client sent: {data}')
-        if message.startswith("CMD"):
-            self.send_command()
+        try:
+            await websocket.send(message)
+            response = await websocket.recv()
+            print(f"< Response from the server: {response}")
+            await asyncio.sleep(ping_pong_interval_sec)
+        except Exception as e:
+            logging.error(f"Error occurred during ping-pong: {e}")
 
     async def get_IoT_ip_address(websocket):
-        remote_ip = websocket.remote_address[0]
+        try:
+            remote_ip = websocket.remote_address[0]
+            return remote_ip
+        except Exception as e:
+            logging.error(f"Error occurred while retrieving remote IP address: {e}")
+            return None
 
+# Task list: 1) G̶e̶n̶n̶e̶m̶g̶a̶n̶g̶.̶ 2) Logik til framehandling + tags/homegrown "metadata" fra videofeed til server. 3) L̶a̶v̶ e̶n̶ c̶o̶m̶m̶a̶n̶d̶ l̶i̶s̶t̶ + m̶a̶n̶a̶g̶e̶m̶e̶n̶t̶.̶ 4) L̶o̶g̶g̶i̶n̶g̶.
 
+# E̶t̶a̶b̶l̶e̶r̶ p̶i̶n̶g̶ c̶o̶n̶n̶e̶c̶t̶i̶o̶n̶ + d̶e̶t̶e̶c̶t̶ m̶o̶t̶i̶o̶n̶ p̶l̶a̶c̶e̶h̶o̶l̶d̶e̶r̶
 
+# Overvej Executor til at wrap video processoren i en stand alone thread.
 
+# Command list: Handshake, camera settings, data transfer, request/response, 
+# Load balancing, error handling, ping/pong, status update, program updates, shutdown/restart.
 
 #Client Class indeholder:
 """  O = first edition done  X = need further details before implementation  ? = unsure if needed
-- __init__             X            improviser i guess
+- __init__             O            improviser i guess
 - publish/subscribe design pattern  se https://github.com/python-websockets/websockets/blob/main/experiments/broadcast/server.py eller https://github.com/python-websockets/websockets/issues/124 4.kommentar
 - get_IoT              ?            Link ubrugeligt 
 - get_IoT_ip_address   X            se https://websockets.readthedocs.io/en/stable/faq/server.html ctrl+f ip ad
@@ -97,9 +156,4 @@ class Client:
 - start_listening      O            se https://community.tempest.earth/t/basic-python-websockets-example-to-retrieve-current-tempest-data/9310/2 basically bare .recv()
 - disconnect           O            se https://websockets.readthedocs.io/en/stable/faq/client.html
 - parse_data           X            se https://pypi.org/project/RPi.GPIO/ pga. https://dataheadhunters.com/academy/how-to-use-python-for-iot-projects-detailed-steps/ ref
-"""
-
-#Client connection management to Server
-"""
-- 
 """
--- homeassistant_websocket/client/video_processing.py
+++ homeassistant_websocket/client/video_processing.py
@@ -1,3 +1,5 @@
+import cv2
+import numpy as np
 # Does processing on the received video data
 # Should respond to the server depending on the type of processing
 
@@ -8,9 +10,89 @@ class VideoProcessor:
         pass
 
     @staticmethod
-    async def motion_detection(message, websocket, clients):
-        # Placeholder for video processing logic
-        pass
+    async def rt_face_recognition(message, websocket): #real time
+        # Make sure to enable camera
+        # Link to guide: https://towardsdatascience.com/real-time-face-recognition-an-end-to-end-project-b738bb0f7348
+        faceCascade = cv2.CascadeClassifier('Cascades/haarcascade_frontalface_default.xml') #THERE MUST BE A CLASSIFIER INSIDE THIS FOLDER!
+        cap = cv2.VideoCapture(0)
+        while(True):
+            ret, img = cap.read()
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+            faces = faceCascade.detectMultiScale(
+                gray,     
+                scaleFactor=1.2,
+                minNeighbors=5,     
+                minSize=(20, 20)
+            )
+            for (x,y,w,h) in faces:
+                cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+                roi_gray = gray[y:y+h, x:x+w]
+                roi_color = img[y:y+h, x:x+w]
+
+            cv2.imshow('video',img)
+    
+            k = cv2.waitKey(30) & 0xff
+            if k == 27: # press 'ESC' to quit
+                break
+        cap.release()
+        cv2.destroyAllWindows()
+
+    @staticmethod
+    async def motion_detection(video_source, message, websocket, clients):
+        cap = cv2.VideoCapture(video_source)
+
+        if not cap.isOpened():
+            print("Error: Unable to open video source")
+            return
+        
+        fgbg = cv2.createBackgroundSubtractorMOG2() #fgbg = foreground/background
+        while True:
+            ret, frame = cap.read()
+
+            if not ret:
+                break
+            fgmask = fgbg.apply(frame) # apply background subtraction -> foreground mask image
+            _, th = cv2.threshold(fgmask, 200, 255, cv2.THRESH_BINARY[1]) #colors above 200 turns white. [1] = thresholded image
+            contours, hierarchy = cv2. findContours(th, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) #RETR_EXTERNAL = external contours, CHAIN... = compress to save memory
+            for contour in contours:
+                area = cv2.contourArea(contour)
+                if area > 100:
+                    x, y, w, h = cv2.boundingRect(contour)
+                    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2) #rectangle drawn around object if area is big enough
+            cv2.imshow('frame', frame) #display frame
+
+            if cv2.waitKey(1) & 0xFF == ord('q'): 
+                break #break loop with 'q'
+        cap.release()
+        cv2.destroyAllWindows()
+
+    @staticmethod
+    async def motion_detected(video_source):
+        cap = cv2.VideoCapture(video_source)
+
+        if not cap.isOpened():
+            print("Error: Unable to open video source")
+            return
+        
+        fgbg = cv2.createBackgroundSubtractorMOG2() #fgbg = foreground/background
+        while True:
+            ret, frame = cap.read()
+
+            if not ret:
+                break
+            fgmask = fgbg.apply(frame) # apply background subtraction -> foreground mask image
+            _, th = cv2.threshold(fgmask, 200, 255, cv2.THRESH_BINARY[1]) #colors above 200 turns white. [1] = thresholded image
+            contours, hierarchy = cv2. findContours(th, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) #RETR_EXTERNAL = external contours, CHAIN... = compress to save memory
+            for contour in contours:
+                area = cv2.contourArea(contour)
+                if area > 100:
+                    return True
+
+        cap.release()
+        cv2.destroyAllWindows()
+
+        return False #not sure if necessary
 
     # Logic to send commands back to server depending on the processing
     @staticmethod
