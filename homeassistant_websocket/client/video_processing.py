68e13a0313ebc068f929536d1a694652a9c161b5
Erik <erik_easy@hotmail.com>
2024-05-09 13:37:58 +0200

    Don't know why but this should follow along as well

--- homeassistant_websocket/client/video_processing.py
+++ homeassistant_websocket/client/video_processing.py
@@ -13,7 +13,103 @@ class VideoProcessor:
         pass
 
     @staticmethod
-    async def camera_adj(video_source)
+    async def camera_adj(video_source):
+        pass
+
+    def face_training():
+        # Path for face image database
+        path = 'dataset'
+        recognizer = cv2.face.LBPHFaceRecognizer_create() # Local Binary Patterns Histogram
+        detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+        # function to get the images and label data
+        def getImagesAndLabels(path):
+            imagePaths = [os.path.join(path,f) for f in os.listdir(path)]
+            faceSamples=[]
+            ids = []
+            for imagePath in imagePaths:
+                PIL_img = Image.open(imagePath).convert('L') # grayscale
+                img_numpy = np.array(PIL_img,'uint8')
+                face_id = (os.path.split(imagePath)[-1].split(".")[1])
+                id_str = face_id.split()[0]
+                id = int(id_str)
+                faces = detector.detectMultiScale(img_numpy)
+                for (x,y,w,h) in faces:
+                    faceSamples.append(img_numpy[y:y+h,x:x+w])
+                    ids.append(id)
+            return faceSamples, ids
+        print ("\n [INFO] Training faces. It will take a few seconds. Wait ...")
+        faces, ids = getImagesAndLabels(path)
+        recognizer.train(faces, np.array(ids))
+        # Save the model into trainer/trainer.yml
+        recognizer.write('trainer/trainer.yml')
+        # Print the numer of faces trained and end program
+        print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
+
+    def face_recognition(name, video_source):
+        recognizer = cv2.face.LBPHFaceRecognizer_create()
+        recognizer.read('trainer/trainer.yml')
+        cascadePath = "haarcascade_frontalface_default.xml"
+        faceCascade = cv2.CascadeClassifier(cascadePath)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        # initiate id counter
+        id = 0
+        # names related to ids: example ==> Marcelo: id=1,  etc
+        names = name 
+        # Initialize and start realtime video capture
+        cam = cv2.VideoCapture(video_source)
+        cam.set(3, 640)  # set video width
+        cam.set(4, 480)  # set video height
+        # Define min window size to be recognized as a face
+        minW = 0.1 * cam.get(3)
+        minH = 0.1 * cam.get(4)
+        while True:
+            ret, img = cam.read()
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+            faces = faceCascade.detectMultiScale(
+                gray,
+                scaleFactor=1.2,
+                minNeighbors=5,
+                minSize=(int(minW), int(minH)),
+            )
+            for (x, y, w, h) in faces:
+                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
+
+                confidence = round(100 - confidence)
+                # If confidence is less them 100 ==> "0" : perfect match
+                if confidence > 20:
+                    id = names[id]
+                else:
+                    id = "unknown"
+
+                cv2.putText(
+                    img,
+                    str(id),
+                    (x + 5, y - 5),
+                    font,
+                    1,
+                    (255, 255, 255),
+                    2
+                )
+                cv2.putText(
+                    img,
+                    f"confidence: {confidence}",
+                    (x + 5, y + h - 5),
+                    font,
+                    1,
+                    (255, 255, 0),
+                    1
+                )
+
+            cv2.imshow('camera', img)
+            k = cv2.waitKey(10) & 0xff  # Press 'ESC' for exiting video
+            if k == 27:
+                break
+        # Do a bit of cleanup
+        print("\n [INFO] Exiting 3/3 Program and cleanup stuff")
+        cam.release()
+        cv2.destroyAllWindows()
 
     @staticmethod
     async def rt_face_recognition(video_source, message, websocket): #real time
@@ -48,13 +144,11 @@ class VideoProcessor:
             elif count >= 30: # Take 30 face sample and stop video
                  break
         # Do a bit of cleanup
-        print("\n [INFO] Exiting Program and cleanup stuff")
-        face_training()
+        print("\n [INFO] Exiting 1/3 Program and cleanup stuff")
         cam.release()
         cv2.destroyAllWindows()
-
-    async def face_training():
-        pass
+        face_training()
+        face_recognizer(name, video_source)
 
     @staticmethod
     async def motion_detection(video_source, message, websocket, clients):
