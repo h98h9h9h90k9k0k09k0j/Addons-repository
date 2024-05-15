df176b414a45e4b2743ae6ce56ff52b611b460e6
AleksanderRDL <aleksander1231234@gmail.com>
2024-05-15 21:46:22 +0200

    Ekstra addon with new camera streaming logic

---
+++ Test-server/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+# Activate the virtual environment
+source /opt/venv/bin/activate
+echo "activated virtual enviroment"
+
+# Start the GStreamer pipeline
+gst-launch-1.0 v4l2src device=/dev/video0 ! video/x-raw,framerate=30/1 ! videoconvert ! omxh264enc ! rtph264pay config-interval=1 ! gdppay ! tcpserversink host=0.0.0.0 port=3030
+
+
+# Start the WebSocket server in the background
+python3 /app/websocket_entry.py &
+echo "started websocket server in background"
+
+# Wait for any process to exit
+echo "waiting for process to exit"
+wait -n
+
+# Exit with status of process that exited first
+echo "exiting"
+exit $?
