6f2f501b59f205db4de9367c549e3f79e4975cc8
AleksanderRDL <aleksander1231234@gmail.com>
2024-05-15 22:30:22 +0200

    Changed encoder

--- Test-server/run.sh
+++ Test-server/run.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -e
 
+# Start the GStreamer pipeline
+gst-launch-1.0 v4l2src device=/dev/video0 ! video/x-raw,framerate=30/1 ! videoconvert ! v4l2h264enc ! rtph264pay config-interval=1 ! gdppay ! tcpserversink host=0.0.0.0 port=3030
+
 # Activate the virtual environment
 source /opt/venv/bin/activate
 echo "activated virtual enviroment"
-
-# Start the GStreamer pipeline
-gst-launch-1.0 v4l2src device=/dev/video0 ! video/x-raw,framerate=30/1 ! videoconvert ! omxh264enc ! rtph264pay config-interval=1 ! gdppay ! tcpserversink host=0.0.0.0 port=3030
-
 
 # Start the WebSocket server in the background
 python3 /app/websocket_entry.py &
