b8246f23f87caa4d5225df211e430ace9201722b
AleksanderRDL <aleksander1231234@gmail.com>
2024-05-16 02:33:31 +0200

    Switched decoder

--- Test-server/run.sh
+++ Test-server/run.sh
@@ -13,14 +13,13 @@ gst-inspect-1.0 gdppay
 gst-inspect-1.0 tcpserversink
 
 # Start the GStreamer pipeline
-gst-launch-1.0 v4l2src device=/dev/video0 ! video/x-raw,framerate=15/1 ! videoconvert ! v4l2h264enc ! rtph264pay config-interval=1 ! gdppay ! tcpserversink host=0.0.0.0 port=8080 
+gst-launch-1.0 v4l2src device=/dev/video0 ! image/jpeg,framerate=30/1 ! videoconvert ! v4l2h264enc ! rtph264pay config-interval=1 ! gdppay ! tcpserversink host=0.0.0.0 port=8080 
 
 # Start the WebSocket server in the background
 python3 /app/websocket_entry.py &
 echo "started websocket server in background"
 
 # Wait for any process to edocker run -it --entrypoint /bin/sh aleksanderrdl/hass-distribute-addon:server_alpine_aarch64
-xit
 echo "waiting for process to exit"
 wait -n
 
