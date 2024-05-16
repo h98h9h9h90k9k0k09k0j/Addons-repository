dc09149defd96e8d0fb580c5a427850e0c76fb2e
AleksanderRDL <aleksander1231234@gmail.com>
2024-05-16 21:48:14 +0200

    More testing

--- FFmpeg-server/run.sh
+++ FFmpeg-server/run.sh
@@ -23,10 +23,10 @@ echo "activated virtual enviroment"
 
 #gst-launch-1.0 v4l2src device=/dev/video0 ! image/jpeg,framerate=30/1 ! jpegdec ! videoconvert ! video/x-raw,format=I420 ! x264enc ! rtph264pay config-interval=1 ! gdppay ! tcpserversink host=0.0.0.0 port=8080 
 
-#GST_DEBUG=4 gst-launch-1.0 v4l2src device=/dev/video0 ! image/jpeg,framerate=30/1 ! jpegdec ! videoconvert ! x264enc ! rtph264pay config-interval=1 ! gdppay ! tcpserversink host=0.0.0.0 port=8080
+GST_DEBUG=4 gst-launch-1.0 v4l2src device=/dev/video0 ! image/jpeg,framerate=30/1 ! jpegdec ! videoconvert ! x264enc ! rtph264pay config-interval=1 ! gdppay ! tcpserversink host=0.0.0.0 port=8080
 
 # test on local
-gst-launch-1.0 v4l2src device=/dev/video0 ! videoconvert ! x264enc ! mp4mux ! filesink location=/addon_configs/capture.mp4
+#gst-launch-1.0 v4l2src device=/dev/video0 ! videoconvert ! x264enc ! mp4mux ! filesink location=/addon_configs/capture.mp4
 
 
 # Start the WebSocket server in the background
---
+++ GStreamer-server/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#set -e
+
+# Activate the virtual environment
+source /opt/venv/bin/activate
+echo "activated virtual enviroment"
+
+# Start FFmpeg for streaming
+ffmpeg -f video4linux2 -i /dev/video0 -vcodec libx264 -f flv rtmp://localhost/live/stream &
+
+# Start the WebSocket server in the background
+python3 /app/websocket_entry.py &
+echo "started websocket server in background"
+
+# Wait for any process to edocker run -it --entrypoint /bin/sh aleksanderrdl/hass-distribute-addon:server_alpine_aarch64
+echo "waiting for process to exit"
+wait -n
+
+# Exit with status of process that exited first
+echo "exiting"
+exit $?
