26d16319a8af67bc438cc5755ef81970f7d50469
AleksanderRDL <aleksander1231234@gmail.com>
2024-05-16 16:52:27 +0200

    Test local videosink

--- Test-server/run.sh
+++ Test-server/run.sh
@@ -17,7 +17,11 @@ echo "activated virtual enviroment"
 # Start the GStreamer pipeline
 #gst-launch-1.0 v4l2src device=/dev/video0 ! videoconvert ! autovideosink
 
-gst-launch-1.0 v4l2src device=/dev/video0 ! image/jpeg,framerate=30/1 ! jpegdec ! videoconvert ! video/x-raw,format=I420 ! x264enc ! rtph264pay config-interval=1 ! gdppay ! tcpserversink host=0.0.0.0 port=8080 
+# test on local
+gst-launch-1.0 v4l2src device=/dev/video0 ! image/jpeg,framerate=30/1 ! jpegdec ! videoconvert ! autovideosink
+
+
+#gst-launch-1.0 v4l2src device=/dev/video0 ! image/jpeg,framerate=30/1 ! jpegdec ! videoconvert ! video/x-raw,format=I420 ! x264enc ! rtph264pay config-interval=1 ! gdppay ! tcpserversink host=0.0.0.0 port=8080 
 
 # Start the WebSocket server in the background
 python3 /app/websocket_entry.py &
