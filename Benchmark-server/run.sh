2bc687553f9b381c1f5d62ae9f843cb2c42d052a
AleksanderRDL <aleksander1231234@gmail.com>
2024-05-27 11:21:00 +0200

    Irrelevant protobuffer for local testing

--- Benchmark-server/run.sh
+++ Benchmark-server/run.sh
@@ -4,10 +4,6 @@
 source /opt/venv/bin/activate
 echo "activated virtual environment"
 
-# Generate the gRPC code from the protobuf definitions
-python -m grpc_tools.protoc -I/app/server --python_out=/app/server --grpc_python_out=/app/server /app/server/workloads.proto
-echo "generated protobuf files"
-
 # Start the main application
 echo "starting the server"
 python /app/server/main.py
